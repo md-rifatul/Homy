e8c86229513c2923657db35be21f0d4ab072e145
Md. Rifatul Islam <rifatuul@gmail.com>
2025-05-04 17:06:40 +0600

    first push

---
+++ Homy/HomyWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HomyWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
