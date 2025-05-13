2119f6462835d761210e9d42f81b7cec251bc59d
Md. Rifatul Islam <rifatuul@gmail.com>
2025-05-14 00:33:22 +0600

    feat: add Villas DbSet to ApplicationDbContext

--- Homy/Homy.Infrastructure/Data/ApplicationDbContext.cs
+++ Homy/Homy.Infrastructure/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Homy.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace Homy.Infrastructure.Data
@@ -11,7 +12,8 @@ namespace Homy.Infrastructure.Data
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
-
         }
+
+        public DbSet<Villa> Villas { get; set; }
     }
 }
