9ced3656d9b3b6657b49aaca3311cb45716e9290
Md. Rifatul Islam <rifatuul@gmail.com>
2025-05-23 12:20:40 +0600

    push villaNumbers

---
+++ Homy/Homy.Infrastructure/Migrations/20250523061728_addVillaNuberToDb.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Homy.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class addVillaNuberToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Villas",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateTable(
+                name: "villaNumbers",
+                columns: table => new
+                {
+                    Villa_Number = table.Column<int>(type: "int", nullable: false),
+                    VillaId = table.Column<int>(type: "int", nullable: false),
+                    SpecialDetails = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_villaNumbers", x => x.Villa_Number);
+                    table.ForeignKey(
+                        name: "FK_villaNumbers_Villas_VillaId",
+                        column: x => x.VillaId,
+                        principalTable: "Villas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "villaNumbers",
+                columns: new[] { "Villa_Number", "SpecialDetails", "VillaId" },
+                values: new object[,]
+                {
+                    { 101, null, 1 },
+                    { 102, null, 1 },
+                    { 103, null, 1 },
+                    { 201, null, 2 },
+                    { 202, null, 2 },
+                    { 203, null, 2 },
+                    { 301, null, 3 },
+                    { 302, null, 3 },
+                    { 303, null, 3 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_villaNumbers_VillaId",
+                table: "villaNumbers",
+                column: "VillaId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "villaNumbers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Villas",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+        }
+    }
+}
